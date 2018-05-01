8adcd3a6411403724bfb85fb40445e1da9a42913
alexisfisher <afisher@cs.wisc.edu>
2018-05-01 11:38:58 -0500

    functioning

---
+++ api.php
@@ -0,0 +1,85 @@
+<?php
+require 'vendor/autoload.php';
+
+$Request = explode('/', trim($_SERVER['REQUEST_URI'],'/'));
+//if the request uri matches index.html or .js, pass through
+if ($Request[0] == "index.html"){
+	return false;
+}
+if ($Request[0] == "behavior.js"){
+	return false;
+}
+if ($Request[0] == "style.css"){
+	return false;
+}
+if ($Request[0] != "api") {
+	echo "Error";
+	return "Error";
+}
+//
+//Request[1] should be the search term
+$code_base="https://restcountries.eu/rest/v2/alpha/";
+$name_base="https://restcountries.eu/rest/v2/name/";
+$url=$code_base.$Request[1];
+$data = getData($url);
+if (!$data){
+	$url=$name_base.$Request[1];
+	$data=getData($url);
+}
+$jsonData=json_decode($data); //is it a list or not?
+$returnData = (object)[];
+$returnData->countries=[];
+$returnData->regions=[];
+$returnData->subregions=[];
+if (!$jsonData){
+	return "None";
+}
+if (!(is_array($jsonData))){
+	$jsonData = [$jsonData];
+}
+foreach($jsonData as $country){
+	//check if we've got over 50 and bail if so
+	if (count($returnData->countries) >=50){
+		break;
+	}
+	array_push($returnData->countries, $country);
+	$region = $country->region;
+	if (key_exists($region,$returnData->regions)){
+		$returnData->regions[$region] += 1;
+	} else{
+		$returnData->regions[$region] = 1;
+	}
+	$subregion = $country->subregion;
+	if (key_exists($subregion, $returnData->subregions)){
+		$returnData->subregions[$subregion] +=1;
+	} else {
+		$returnData->subregions[$subregion] =1;
+	}
+}
+usort($returnData->countries, "countryCmp");
+
+$returnData->countryCount = count($returnData->countries);
+
+echo json_encode($returnData);
+
+function getData($url){
+	try{
+		$guzzle=new GuzzleHttp\Client();
+
+		$response= $guzzle->request('GET',$url, ["query"=> ["fields"=> "name;alpha2Code;alpha3Code;flag;region;subregion;population;languages"]]);
+		$data=$response->getBody()->getContents();
+		return $data;
+	} catch (Exception $e) {
+	}
+}
+
+function countryCmp($a,$b){
+	if (strcmp($a->name, $b->name) == 0) {
+		if ($a->population == $b->population) {
+			return 0;
+		}
+		return ($a->population < $b->population) ? -1 : 1;
+	}
+	return strcmp($a->name, $b->name);
+}
+?>
---
+++ behavior.js
@@ -0,0 +1,110 @@
+/*  behavior.js
+ *  Alexis Fisher
+ *  alexis.fisher@gmail.com
+ *  5/1/2018
+ *  
+ *  performs the search and displays results
+ *
+ */
+function search(){
+	//get the input
+	var searchString = document.getElementById("search").value.trim();
+	//validate input
+	if (searchString.length == 0){
+		//shortcut
+		displayError();
+		exit;
+	}
+	hideError();
+	//do the search
+	results = doSearch(searchString);	
+	//call results with the result json
+	displayResults(results);
+}
+
+function doSearch(searchString = ""){
+	//
+	//construct request
+	var req = new XMLHttpRequest();
+	var requestURL="/api/"+searchString;
+	req.open("GET",requestURL, false);
+	req.send();
+	return req.responseText;
+
+}
+
+function displayResults(results){
+	hideError();
+	var resultsDiv = document.getElementById("results");
+	resultsDiv.innerHTML="";
+	resultsDiv.style.display = "block";
+	if (results.length == 0) {
+		displayError();
+		exit;
+	}
+	var data = JSON.parse(results);
+	//name;alpha2Code;alpha3Code;flag;region;subregion;population;languages
+	var table=" <table> <tr><th>Flag</th> <th>Country</th> <th>Code 2</th> <th>Code 3</th>";
+	table+="<th>Region</th> <th>Subregion</th> <th>Population</th> <th>Languages</th></tr> ";
+	for(var i = 0; i < data.countries.length; i++){
+		var country = data.countries[i];
+		var row= "<tr> <td> <img src="+country.flag +"></td>"; 
+		row += "<td>"+country.name + "</td><td>" + country.alpha2Code + "</td><td>";
+		row += country.alpha3Code + "</td><td>" + country.region + "</td><td>" + country.subregion;
+		row += "</td><td>" + country.population + "</td><td>";
+		for (var l = 0; l < country.languages.length; l++){
+			row += country.languages[l].name;
+			if (l < country.languages.length -1 ){
+				row += ", ";
+			}
+		}
+		row += "</td> </tr>";
+
+		table += row;
+	}
+	table+="</table>";
+	resultsDiv.innerHTML = table;
+	if (data.countryCount > 1){
+		resultsDiv.innerHTML+="<h3>"+data.countryCount +" Countries</h3>";
+	}else{
+		resultsDiv.innerHTML+="<br>" + "<h3>1 Country</h3>";
+	}
+	table = "<table><tr> <th>Region</th> <th>Count</th></tr>";
+	for(var key in data.regions)
+	{
+		table+="<tr><td>" + key + "</td><td>" + data.regions[key] + "</td></tr>";
+	}
+	table += "</table>"
+	resultsDiv.innerHTML+= table;
+
+	table = "<table><tr> <th>Subregion</th> <th>Count</th></tr>";
+	for (var key in data.subregions){
+		table+="<tr><td>" + key + "</td><td>" + data.subregions[key] + "</td></tr>";
+	}
+	table += "</table>"
+	resultsDiv.innerHTML+= table;
+	
+}
+function hideResults(){
+	var resultsDiv = document.getElementById("results");
+	resultsDiv.innerHTML="";
+	resultsDiv.style.display = "none";
+}
+
+function displayError(){
+	var errorDiv = document.getElementById("error");
+	hideResults();
+	errorDiv.innerHTML = "<h2>Error!</h2>"
+	errorDiv.style.display = "block";
+
+}
+function hideError(){
+	var errorDiv = document.getElementById("error");
+	errorDiv.innerHTML = "";
+	errorDiv.style.display = "none";
+}
+
+function onBodyLoad(){
+	hideError();
+	hideResults();
+}
